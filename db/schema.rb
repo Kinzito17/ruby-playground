e0518c9f94c5288e20be8e31d2c6f0c068de0f60
Matthew Kinzle <matthew.kinzle@trustedhealth.com>
2023-01-03 16:08:34 -0600

    db and stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_03_202940) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_03_211719) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
