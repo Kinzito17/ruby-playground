a62a78e4ace7c02d2090543ab305e25023255c69
Matthew Kinzle <matthew.kinzle@trustedhealth.com>
2023-01-04 16:58:33 -0600

    updating

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,36 @@ class ArticlesController <  ApplicationController
       @article = Article.find(params[:id])
     end
 
+    def index
+      @articles = Article.all
+    end
+
+    def new
+      @article = Article.new
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def create
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      if @article.save
+        flash[:notice] = "Article has been created successfully"
+        redirect_to @article
+      else
+        render :new, status: 422      
+      end
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = "Article has been edited successfully"
+        redirect_to @article
+      else
+        render :edit, status: 422      
+      end
+    end
+
 end
