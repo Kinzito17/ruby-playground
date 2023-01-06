a3237f4bf5a257f196eccd39d88881a2a15e72f4
Matthew Kinzle <matthew.kinzle@trustedhealth.com>
2023-01-06 16:30:41 -0600

    adding user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController <  ApplicationController
     def new
     @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome #{@user.username}, your account has been created successfully"
+            redirect_to articles_path
+          else
+            render :new, status: 422      
+          end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+  
 end
