1dc498fb6620244ac91ecb5905e241a42e9d2673
Matthew Kinzle <matthew.kinzle@trustedhealth.com>
2023-01-08 12:15:58 -0600

    conditionally rendering actions for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController <  ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome #{@user.username}, your account has been created successfully"
             redirect_to articles_path
           else
