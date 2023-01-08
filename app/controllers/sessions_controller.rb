14ac8d786c2f4d270905f2e212a70737aae9a28b
Matthew Kinzle <matthew.kinzle@trustedhealth.com>
2023-01-08 10:29:51 -0600

    logging out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController <  ApplicationController
+
+def new
+end
+
+def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        flash[:notice] = "You have logged in successfully"
+        redirect_to user
+    else
+        flash.now[:alert] = "There was an issue with your login credentials"
+        render :new, status: 422   
+    end
+end
+
+def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about' 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :articles, :users
 
 end
